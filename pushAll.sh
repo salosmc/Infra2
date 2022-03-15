60a40e03054c2cf03fb6ba2464a8a8e202480e8f
salosmc <salo_smc@hotmail.com>
2022-03-14 22:30:37 -0300

    Actualizacion

--- pushAll.sh
+++ pushAll.sh
@@ -10,36 +10,36 @@
 # uptogit <ficheros>
 
 # Comprobamos si el directorio en el que estamos es de un repositorio git
-#if [ ! -d '.git' ]; then
-#	echo 'Esta carpeta no contiene un repositorio Git'
-#	exit -1
-#fi
+if [ ! -d '.git' ]; then
+	echo 'Esta carpeta no contiene un repositorio Git'
+	exit -1
+fi
 
 # Ahora comprobamos si se le paso algun parametro
-#if [ $# == 0 ]; then
-#	echo "UpToGit: ¡Error! No se le a pasado ningún parámetro"
-#	echo "uptogit fichero1 fichero2 ... ficheroN"
-#	exit -1
-#else
+if [ $# == 0 ]; then
+	echo "UpToGit: ¡Error! No se le a pasado ningún parámetro"
+	echo "uptogit fichero1 fichero2 ... ficheroN"
+	exit -1
+else
 	# Recorremos los parametros para comprobar si son ficheros o directorios
-	#for file in $*; do
-	#	if [ ! -e $file ]; then
-	#		echo "UpToGit: El archivo o directorio $file no existe"
-	#		exit -1
-	#	fi
-	#done
+	for file in $*; do
+		if [ ! -e $file ]; then
+			echo "UpToGit: El archivo o directorio $file no existe"
+			exit -1
+		fi
+	done
 	
 	# Si llegamos hasta aquí, indicamos a Git los archivos a subir
-	#git add $*
-	git add .
+	git add $*
+	#git add .
 
 	# Esto nos pedira el mensaje del commit
-	#echo "Introduce el mensaje del commit:"
-	#read TXT
-	#git commit -m "$TXT"
-    git commit -m "actualizacion"
+	echo "Introduce el mensaje del commit:"
+	read TXT
+	git commit -m "$TXT"
+    #git commit -m "actualizacion"
 
 	# Y terminamos subiendo los archivos
 	git push origin master
 
-#fi
+fi
