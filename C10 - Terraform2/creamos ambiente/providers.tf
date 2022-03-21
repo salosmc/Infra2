3f55e26aef7bea37b14b8ea35b006cac429c3a3b
salosmc <salo_smc@hotmail.com>
2022-03-21 12:02:36 -0300

    actualizacion

--- C10 - Terraform2/creamos ambiente/providers.tf
+++ C10 - Terraform2/creamos ambiente/providers.tf
@@ -1,3 +1,5 @@
+/*
+
 # ==================================================================
 # Propósito: declaramos que proveedor cloud queremos usar
 # Autor: DH
@@ -28,10 +30,50 @@ provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
-/*
-provider "aws" {
-    shared_credentials_file = "~/.aws/credentials"
-    region = "us-east-1"
-}
-*/
+
+#provider "aws" {
+#   shared_credentials_file = "~/.aws/credentials"
+#    region = "us-east-1"
+#}
+
 # ==================================================================
+*/
+# Propósito: declaramos que proveedor cloud queremos usar
+
+# Autor: DH
+
+# Fecha: 30.07.21
+
+# Versión: 1.0
+
+# Declaramos el Cloud Provider con el que queremos trabajar
+
+terraform {
+
+# Le decimos que queremos:
+# a. la versión del binario de terraform mayor o igual a 0.12
+
+    required_version = ">=0.12"
+    required_providers {
+
+        aws = {
+# Especificamos desde donde queremos descargar el binario:
+
+        source = "hashicorp/aws"
+# Le decimos que solo permitirá:
+# b. la versión del binario del provider 3.20.0 (con cierta restricción)
+        version = "~> 3.20.0"
+
+    }
+}
+}
+
+# Declaramos la región donde queremos levantar nuestra infra
+
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+
--- C10 - Terraform2/creamos ambiente/variables.tf
+++ C10 - Terraform2/creamos ambiente/variables.tf
@@ -1,3 +1,5 @@
+/*
+
 # ==================================================================
 # Proposito: declaramos todas las variables que vamos a usar
 # Autor: DH
@@ -5,7 +7,7 @@
 # Version: 1.0
 # ==================================================================
 
-/*Varialbes provider*/
+#Varialbes provider
 variable "access_key" {
     type = string
     default = "AKIAWB4P7DBDRNZBIMNQ"
@@ -42,3 +44,48 @@ variable "private_subnets" {
     default = "10.0.0.192/26"
 }
 # ==================================================================
+
+*/
+
+variable "access_key" {
+    type = string
+    default = "AKIAWB4P7DBD5HMJPLP5"
+    }
+variable "secret_key" {
+    type = string
+    default = "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"
+    }
+variable "region" {
+    type = string
+    default = "us-east-1"
+    }
+
+variable "aws_region_id" {
+    description = "la region"
+    type = string
+    default = "us-east-1"
+
+    }
+
+variable "main_vpc_cidr" {
+
+    description = "Nuestro Security Group"
+    type = string
+    default = "10.0.0.0/24"
+
+}
+
+variable "public_subnets" {
+
+    description = "subnet con acceso a internet"
+    type = string
+    default = "10.0.0.128/26"
+}
+
+variable "private_subnets" {
+
+description = "subnet sin acceso a internet"
+type = string
+default = "10.0.0.192/26"
+
+}
