e689ebbbe9833bf44ebe50615a7f310eb63e67d3
salosmc <salo_smc@hotmail.com>
2022-03-21 11:48:29 -0300

    actualizacion

---
+++ C10 - Terraform2/creamos ambiente/variables.tf
@@ -0,0 +1,44 @@
+# ==================================================================
+# Proposito: declaramos todas las variables que vamos a usar
+# Autor: DH
+# Fecha: 30.07.21
+# Version: 1.0
+# ==================================================================
+
+/*Varialbes provider*/
+variable "access_key" {
+    type = string
+    default = "AKIAWB4P7DBDRNZBIMNQ"
+    }
+variable "secret_key" {
+    type = string
+    default = "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"
+    }
+variable "region" {
+    type = string
+    default = "us-east-1"
+    }
+
+
+
+variable "aws_region_id" {
+    description = "la region"
+    type = string
+    default = "us-east-1"
+}
+variable "main_vpc_cidr" {
+    description = "Nuestro Security Group"
+    type = string
+    default = "10.0.0.0/24"
+}
+variable "public_subnets" {
+    description = "subnet con acceso a internet"
+    type = string
+    default = "10.0.0.128/26"
+}
+variable "private_subnets" {
+    description = "subnet sin acceso a internet"
+    type = string
+    default = "10.0.0.192/26"
+}
+# ==================================================================
--- C8 - terraform/Crear-grPub-grPiv/variables.tf
+++ C8 - terraform/Crear-grPub-grPiv/variables.tf
@@ -1,14 +1,22 @@
+/*Varialbes provider*/
+variable "access_key" {
+    type = string
+    default = "AKIAWB4P7DBDRNZBIMNQ"
+    }
+variable "secret_key" {
+    type = string
+    default = "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"
+    }
+variable "region" {
+    type = string
+    default = "us-east-1"
+    }
 
 /*Variables para la pvc*/
 
-variable vpc-hakuna {
+variable "namespace" {
   type        = string
   default     = "hakuna"
 }
 
-variable name {
-  type        = string
-  default     = ""
-  description = "description"
-}
 
